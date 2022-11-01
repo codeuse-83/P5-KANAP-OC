73603a08e1f627018d9020bb1951e35aeb01df02
Stef <stephaniebinther@gmail.com>
2022-11-01 14:37:12 +0100

    Récupération de l' API, et Fonction pour l'affichage

--- front/js/script.js
+++ front/js/script.js
@@ -1,38 +1,23 @@
-const sectionItems = document.getElementById("items");
+let items_container = document.querySelector(".items");
 
-getData();
+// Récupération de l'API et envoie une requête au back-end
+fetch(`http://localhost:3000/api/products`)
+  .then((products) => products.json())
+  .then((products) => {
+    displayProducts(products);
+  });
 
-// Récupération de l'API et envoie une requête au back-end //
-async function getData () {
-  fetch("http://localhost:3000/api/products")
-    // Récupération des données de l'API dans un fichier.json //
-    .then((response) => response.json())
-    //  Données mises dans la variable value //
-    .then((value) => displayItems(value))
-    //Si une erreure se produit un message s'affichera //
-    .catch(() => {
-      alert("Aucun article diponinle ce jour.<br>Merci de réessayer d'ici quelques jours ")
-    });
-};
-
-// Fonction pour afficher les élements HTML //
-function displayItems(value) {
-  let displayHtml = "";
-  //Boucle pour chaque article que l'on récupère //
-  for (let product of value) {
-    // C'est ici que les canapés s'affichent //
-    displayHtml +=
-      (`<a href="./product.html?id=${product._id}">
-    <article>
-    <img src="${product.imageUrl}" alt="${product.altTxt}"/>
-    <h3 class='productName'>${product.name}</h3>
-    <p class='productDescription'>${product.description}</p>
-    </article>
-    </a>`);
-  };
-  // Affichage dans la section items //
-  sectionItems.insertAdjacentHTML("afterbegin", displayHtml);
-};
-
-
-  
+//Création des élèments + rajout des données dans ces balises
+function displayProducts(products) {
+  items_container.innerHTML = "";
+  for (let i = 0; i < products.length; i++) {
+    items_container.innerHTML += `
+    <a href="./product.html?_id=${products[i]._id}"> 
+      <article>
+        <img src="${products[i].imageUrl}" alt="${products[i].altTxt}">
+        <h3 class="productName">${products[i].name}</h3> 
+        <p class="productDescription">${products[i].description}</p>
+      </article>
+    </a>`;
+  }
+}
